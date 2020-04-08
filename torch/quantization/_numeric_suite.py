d894e60aeb1c64c5fd3bcc1b73625ceae36b46b1
Haixin Liu <haixin@fb.com>
2020-04-07 20:12:44 -0700

    [PyTorch Numeric Suite] Add weight compare API
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/36186
    
    Start PyTorch Numeric Suite under PyTorch quantization and add weight compare API to it.
    ghstack-source-id: 101731958
    
    Differential Revision: [D20903395](https://our.internmc.facebook.com/intern/diff/D20903395/)

---
+++ torch/quantization/_numeric_suite.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+def _find_match(str_list, key_str, postfix):
+    split_str = key_str.split(".")
+    if split_str[-1] == postfix:
+        match_string = "".join(key_str.split(".")[0:-1])
+        for s2 in str_list:
+            pattern1 = "".join(s2.split(".")[0:-1])
+            pattern2 = "".join(s2.split(".")[0:-2])
+            if match_string == pattern1:
+                return s2
+            if match_string == pattern2:
+                return s2
+    else:
+        return None
+
+
+def compare_weights(float_dict, quantized_dict):
+    r"""Returns a dict with key corresponding to module names and each entry being
+    a dictionary with two keys 'float' and 'quantized', containing the float and
+    quantized weights
+    Args:
+        float_dict: state dict of the float model
+        quantized_dict: state dict of the quantized model
+    """
+    weight_dict = {}
+    for key in quantized_dict:
+        match_key = _find_match(float_dict, key, "weight")
+        if match_key is not None:
+            weight_dict[key] = {}
+            weight_dict[key]["float"] = float_dict[match_key]
+            weight_dict[key]["quantized"] = quantized_dict[key]
+    return weight_dict
